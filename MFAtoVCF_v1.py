016d1ae32a8647e4ebc45297eac548f578a5b96f
Roger <roger.mulet@gmail.com>
2016-03-17 13:03:22 +0100

    MFAtoVCF - One lined FASTA

--- MFAtoVCF_v1.py
+++ MFAtoVCF_v1.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# MFAtoVCF.py, Version 1.1
-
 # This script converts multi-FASTA alignments (.mfa) files structured as pairs of aligned sequences,
 # each of which covers a region of a chromosome. Since the data from the 1000 GP is divided into chromosomes, 
 # it is also convenient to have the mouse-human alignments as several VCFs for their subsequent merging.
@@ -22,7 +20,6 @@ parser = argparse.ArgumentParser(description='Obtain a VCF file from a MFA using
 parser.add_argument('-i','--input',type=str,required=True,help="Input file, in MFA format")
 parser.add_argument('-o','--output',type=str,default="output.vcf",help="Output file, in VCF format")
 parser.add_argument('-c','--chrom',type=str,default="22",help="Chromosome identifier")
-parser.add_argument('-t','--test',action='store_true',help="Testing mode, keeps temporal files for examination")
 
 args = parser.parse_args()
 
@@ -31,12 +28,12 @@ args = parser.parse_args()
 ############################
 
 # VCF MANIPULATION:
-trigger = 0 # Trigger=0 - Start; Trigger=1 - 1st human seq; Trigger=2 - 1st mouse seq; Trigger=3 - 2nd human seq
+trigger = 0
 counter = 0
 templines = ""
 totalrows = ""
 # VCF FILE HEADER:
-startpos = ""
+header = ""
 version = ""
 chrom = args.chrom
 seqlength = 0
@@ -64,24 +61,21 @@ def vcfprocess(seqlength,totalrows):
 
 with open(args.input,'r') as file: 
 	for line in file:
-		if line[0]==">": 
+		if line[0]==">":
 			trigger+=1
-		if trigger==3: # Every Human (or reference species) line after the first
+		if trigger==3:
 		# TAKES THE TWO FASTA SEQUENCES AND TRANSFORM TO VCF FILE	
 			seqlength,totalrows = vcfprocess(seqlength,totalrows)
 			trigger=1 # Sets the trigger to 1 again					
 			templines = "" # Resets the templines variable to empty
 			counter+=1
 		# STORES THE POSITION OF THE CURRENT FRAGMENT:
-		if trigger == 1 and line[0]==">": # Stores the length of every aligned region
+		if trigger == 1: # Stores the length of every aligned region
 			match = re.search(r'(chr\d+:)(\d+)-',line)	# I've replaced \d\d with \d+
-			if match: startpos = match.group(2)
-			templines=templines+line
-		elif trigger == 2 and line[0]==">":
-			templines=templines+'\n'+line	
-		else:	
-			templines=templines+line.strip()
- 	# UPON REACHIN THE END OF THE
+			if match: header = match.group(2)
+		# TEMPORARILY STORES THE CURRENT LINE:
+		templines=templines+line
+ 	# END OF FILE:
 	seqlength,totalrows = vcfprocess(seqlength,totalrows)
 	counter+=1	
 	templines = ""
@@ -96,9 +90,8 @@ out.write('##contig=<ID=%s,length=%d>\n##FORMAT=<ID=GT,Number=1,Type=String,Desc
 out.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tHuman\tMouse')
 out.write(totalrows)
 # Deleting temporal files required for SNP-sites
-if not args.test:
-	os.remove("tempin.fasta")
-	os.remove("tempout.vcf")
+os.remove("tempin.fasta")
+os.remove("tempout.vcf")
 
 print("Execution complete. %d FASTA pair(s) processed." %counter)
 exit()
